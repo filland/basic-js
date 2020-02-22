09e7e9e0bf0167aa8e5bfd893ced2737ada962a6
filland <kubalexei@gmail.com>
2020-02-22 23:44:46 +0300

    feat: impl what season task

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,36 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  
+  if(date === undefined) {
+    return 'Unable to determine the time of year!';
+  }
+
+  try {
+    date.toUTCString();
+  } catch (e) {
+    throw new Error();
+  }
+  const winter = "winter";
+  const spring = "spring";
+  const summer  = "summer";
+  const fall = "fall";
+
+  const seasons = {
+    1: winter,
+    2: winter,
+    3: spring,
+    4: spring,
+    5: spring,
+    6: summer,
+    7: summer,
+    8: summer,
+    9: fall,
+    10: fall,
+    11: fall,
+    12: winter
+  }
+
+  // const monthNames = ["January", "February", "March", "April", "May", "June",
+    // "July", "August", "September", "October", "November", "December"
+  // ];
+  return seasons[date.getMonth()+1];
 };
