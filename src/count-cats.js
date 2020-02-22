e463470abfeac40b79277429275e93601bc671f1
filland <kubalexei@gmail.com>
2020-02-22 23:29:19 +0300

    feat: implement count cats and dreamteam tasks

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,5 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  if(matrix === undefined || matrix === null || matrix.length === 0) return 0;
+  let matchResult = (";;"+matrix.map(array => array.join(";;")).join(";;")+';;').match(/;\^\^;/g);
+  return matchResult === null ? 0 : matchResult.length;
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,16 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split("");
+  if(members === undefined || members === null || !Array.isArray(members) || members.length === 0) {
+    return false;
+  }
+    return dreamteam = members
+    .filter(member => typeof member === 'string')
+    .filter(member => alphabet.indexOf(member.trim().charAt(0).toLowerCase()) !== -1)
+    .reduce((sum, member) => {
+      sum.push(member.trim().charAt(0).toLowerCase());
+      return sum;
+    }, [])
+    .sort()
+    .join("")
+    .toUpperCase(); 
 };
