12d67bee4e44eca20b5978b3bf43e56c80dea8b2
filland <kubalexei@gmail.com>
2020-03-04 21:41:26 +0300

    feat: impl extended repeat

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,9 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {    
+    let separator = options.hasOwnProperty("separator") ? options.separator : '+';
+    let additionSeparator = options.hasOwnProperty("additionSeparator") ? options.additionSeparator : '|';
+    let addition =
+        options.hasOwnProperty("additionRepeatTimes") ? 
+            new Array(options.additionRepeatTimes)
+                .fill(options.addition === null ? "null" : options.addition).join(additionSeparator) : "";
+    return new Array(options.repeatTimes).fill(new String(str)).join(addition+separator)+addition;
 };
-  
